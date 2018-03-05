3ab839bba8877a89570c85d3741a70de2f33c703
Denis Lupu <foolcat@protonmail.ch>
2018-03-05 18:37:25 +0200

    Add signup page mockup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class Home extends React.Component {
-	render() {
-		return (
-			<div>
-				I'm Home page
-			</div>
-		)
-	};
+  render() {
+    return (
+      <div>
+        I'm Home page
+      </div>
+    )
+  };
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
