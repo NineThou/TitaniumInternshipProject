764d1958b4771ddb5b9a67bc0997d339a7f30d4b
Denis Lupu <foolcat@protonmail.ch>
2018-03-06 09:26:44 +0200

    Add mockup registration page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstPassword: '',
+      secondPassword: '',
+    }
+  }
+
+
   render() {
     return (
       <div>
-        I'm SignUp page
+        <form class="ui form">
+          <div class="field">
+            <label>E-mail</label>
+            <input placeholder="E-mail" type="email" />
+          </div>
+          <div class="field">
+            <label>Password</label>
+            <input placeholder="Password" type="password" ref={(input) => }/>
+          </div>
+          <div class="field">
+            <label>Repeat password</label>
+            <input placeholder="Repeat password" type="password" />
+          </div>
+          <button type="submit" class="ui button" role="button">Submit</button>
+        </form>
       </div>
     )
   };
