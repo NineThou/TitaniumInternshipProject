9d715d8a7bb1fb708c80f8033370aea5c247b9b7
NineThou <shelkov7000@gmail.com>
2018-03-08 19:58:18 +0200

    Signup page layout fix

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,46 +3,64 @@ import { Button, Form } from 'semantic-ui-react';
 import styled from 'react-emotion';
 
 const SignUpWrap = styled('div')`
-  margin: 0 auto;
   display: flex;
   flex-direction: column;
-  background-color: #B2BABB;
+  width: 380px;
+  height: 280px;
+  margin: 0 auto;
+  position: relative;
+  top: 50px;
+  color: black;
   border: 3px solid #586180;
-  width: 350px;
-  padding: 10px;
   border-radius: 5px;
+  background-color: #B2BABB;
+  padding-top: 5px;
 `;
 
 const ContainerWrap = styled('Container')`
   margin: 0 auto;
 `;
 
+const MainWrap = styled('div')`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled('h1')`
+  margin: 0 auto;
+`;
+
 const SignUp = () => (
-  <SignUpWrap>
-    <ContainerWrap>
-      <Form size="large" widths="equal">
-        <Form.Field>
-          <label htmlFor="email">
-          E-mail
-            <input placeholder="E-mail" id="email" type="email" />
-          </label>
-        </Form.Field>
-        <Form.Field>
-          <label htmlFor="password">
-          Password
-            <input placeholder="Password" type="password" />
-          </label>
-        </Form.Field>
-        <Form.Field>
-          <label htmlFor="password">
-          Repeat password
-            <input placeholder="Repeat password" type="password" id="password" />
-          </label>
-        </Form.Field>
-        <Button type="submit">Submit</Button>
-      </Form>
-    </ContainerWrap>
-  </SignUpWrap>
+  <MainWrap>
+    <Title>Registration</Title>
+    <SignUpWrap>
+      <ContainerWrap>
+        <Form size="large" widths="equal">
+          <Form.Field>
+            <label htmlFor="email">
+            E-mail
+              <input placeholder="E-mail" id="email" type="email" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="password">
+            Password
+              <input placeholder="Password" type="password" />
+            </label>
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor="password">
+            Repeat password
+              <input placeholder="Repeat password" type="password" id="password" />
+            </label>
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </ContainerWrap>
+    </SignUpWrap>
+  </MainWrap>
 );
 
 
