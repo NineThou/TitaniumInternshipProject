a081cf497eb5eed12f931696715866f09b2986b7
John Lyon <lyon.john.jr@gmail.com>
2018-03-13 14:01:44 +0200

    added lint script, autofixed lint errors with --fix

--- src/utils/table.jsx
+++ src/utils/table.jsx
@@ -25,16 +25,17 @@ const newPerson = () => {
 };
 
 export function makeData(len = 5553) {
-  return range(len).map((d) => {
-    return {
-      ...newPerson(),
-      children: range(10).map(newPerson)
-    };
-  });
+  return range(len).map(d => ({
+    ...newPerson(),
+    children: range(10).map(newPerson),
+  }));
 }
 
 export const Logo = () => (
-  <div style={{ margin: '1rem auto', display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center'}}>
+  <div style={{
+ margin: '1rem auto', display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center',
+}}
+  >
     For more examples, visit {''}
     <br />
     <a href="https://github.com/react-tools/react-table" target="_blank">
