abb5dc7a1a76c607f76f5be79727239a00a3b660
Denis <denis.lupu@titanium-soft.com>
2018-03-13 17:30:32 +0200

    Intl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { addLocaleData, IntlProvider } from 'react-intl';
 import 'semantic-ui-css/semantic.min.css';
-
+import messages from './utils/messages';
+import flattenMessages from './utils/flatten';
 // routes
-import router from './constants/routes';
+import Router from './constants/routes';
 
 // css
 import './styles/App.css';
@@ -10,5 +13,18 @@ import './styles/App.css';
 // service worker
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(router, global.document.getElementById('root'));
+// locale data
+import en from 'react-intl/locale-data/en';
+import ru from 'react-intl/locale-data/ru';
+
+
+addLocaleData([...en, ...ru]);
+
+const locale = 'en-US';
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+    <Router />
+  </IntlProvider>
+  , global.document.getElementById('root'));
 registerServiceWorker();
---
+++ src/utils/messages.js
@@ -0,0 +1,8 @@
+export default {
+  'en-US': {
+    detail: {
+      toggle: 'test',
+    },
+  },
+
+};
