7975bca1ffd816b9282374243065e801be535abd
foolcat <foolcat@protonmail.ch>
2018-03-28 10:38:59 +0300

    Fixed merge conflict

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,13 +1,8 @@
 import { fork, all } from 'redux-saga/effects';
 
 import watchUsersData from './users';
-<<<<<<< HEAD
 import watchEventsData, { setEventData, removeEvent } from './events';
-import watchPostsData, { setPostData, removePost } from './posts';
-=======
-import watchEventsData, { setEventData } from './events';
 import watchPostsData, { setPostData, removePost, submitLikeToPost, removeLikeFromPost } from './posts';
->>>>>>> ee432ab73b8532b8716613a0f2130868d8faa2fc
 
 
 export default function* rootSaga() {
@@ -18,11 +13,8 @@ export default function* rootSaga() {
     fork(setPostData),
     fork(setEventData),
     fork(removePost),
-<<<<<<< HEAD
     fork(removeEvent),
-=======
     fork(submitLikeToPost),
     fork(removeLikeFromPost),
->>>>>>> ee432ab73b8532b8716613a0f2130868d8faa2fc
   ]);
 }
